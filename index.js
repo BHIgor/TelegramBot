42ee405fbaa4ee968fec61f4ef65103881111c68
igor <luiz23@ukr.net>
2021-12-17 19:59:22 +0300

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api')
 const kb = require('./keyboard-buttons')
 const keyboard = require('./keyboard')
 const helper = require('./helper')
-
+//ыыыы
 helper.logStart()
 
 const TOKEN = "5033990491:AAGKChEe_nf0SpyjZLb-QxKKw9E8YhpWO5s"
@@ -20,7 +20,7 @@ bot.on('message', msg => {
             })
             break
         case kb.home.cinemas:
-            break
+            break 
         case kb.back:
             bot.sendMessage(chatId, `Что хотите посмотреть?`,{
                 reply_markup:{keyboard:keyboard.home}
