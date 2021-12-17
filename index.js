3572e386b7c0d489b29ebdee119975f32b54f0c1
igor <luiz23@ukr.net>
2021-12-17 16:13:57 +0300

    Додали перші кнопки

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const TelegramBot = require('node-telegram-bot-api')
+const kb = require('./keyboard-buttons')
+const keyboard = require('./keyboard')
+
 
 const TOKEN = "5033990491:AAGKChEe_nf0SpyjZLb-QxKKw9E8YhpWO5s"
 const bot = new TelegramBot(TOKEN, {polling:true})
 
+
 bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, `hello s15551o2bawe112ra${msg.from.first_name}`)
+    switch (msg.text){
+
+    }
+})
+
+bot.onText(/\start/, msg => {
+    const text = `Привет ${msg.from.first_name}, я готов к работе`
+    bot.sendMessage(msg.chat.id, text, {
+        reply_markup:{
+            keyboard:keyboard.home
+        }
+    })
 })
---
+++ keyboard-buttons.js
@@ -0,0 +1,10 @@
+module.exports = {
+    home:{
+        films:'Сейчас в кино',
+        favorite:'Избранное',
+        cinemas:'Кинотеатры'
+    },
+    film:{
+
+    }
+}
