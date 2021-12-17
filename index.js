19cdc75be3081441f071a9af286dbe72b3820ed2
igor <luiz23@ukr.net>
2021-12-17 16:42:11 +0300

    fix

--- index.js
+++ index.js
@@ -10,17 +10,16 @@ const bot = new TelegramBot(TOKEN, {polling:true})
 bot.on('message', msg => {
     switch (msg.text){
         case kb.home.favorite:
-            
             break
         case kb.home.films:
             break
-        case kb.home.cinemas:
+        case kb.home.cine:
             break
                     
     }
 })
 
-bot.start((msg) => {
+bot.onText(/\/start/, msg => {
     const text = `Привет ${msg.from.first_name}, я готов к работе`
     bot.sendMessage(msg.chat.id, text, {
         reply_markup:{
