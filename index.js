9c0ff2eea2cceb148c80241c42c734cb526e118f
igor <luiz23@ukr.net>
2021-12-17 15:27:37 +0300

    fix

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import TelegramBot from 'node-telegram-bot-api'
+const TelegramBot = require('node-telegram-bot-api')
 
 const TOKEN = "5033990491:AAGKChEe_nf0SpyjZLb-QxKKw9E8YhpWO5s"
 const bot = new TelegramBot(TOKEN, {polling:true})
 
-bot.on('message', msg =>{
-    bot.sendMessage(msg.chat.id,`hello sobara${msg.from.first_name}`)
+bot.on('message', msg => {
+    bot.sendMessage(msg.chat.id, `hello sobara${msg.from.first_name}`)
 })
