03db7d79c0aa36ee6da83ff1d96926c2621fc05b
igor <luiz23@ukr.net>
2021-12-17 15:32:23 +0300

    test

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const TOKEN = "5033990491:AAGKChEe_nf0SpyjZLb-QxKKw9E8YhpWO5s"
 const bot = new TelegramBot(TOKEN, {polling:true})
 
 bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, `hello sobara${msg.from.first_name}`)
+    bot.sendMessage(msg.chat.id, `hello sobawe112ra${msg.from.first_name}`)
 })
