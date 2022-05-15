33d6df90f5c1ff6dcb976496b5d65d4f97aa6db8
igor <luiz23@ukr.net>
2022-05-15 15:16:08 +0300

    fix

--- helper.js
+++ helper.js
@@ -4,6 +4,9 @@ module.exports = {
     logStart(){
         console.log('bot has been')    
     },  
+    stop(){
+        console.log('bot has beensaddassad')    
+    },
     getChatId(msg) {
         return msg.chat.id
     }
