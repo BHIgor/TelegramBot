e586f3e176189b70d52ee1127c39d586a78b0177
igor <luiz23@ukr.net>
2022-05-15 14:24:32 +0300

    fix

--- helper.js
+++ helper.js
@@ -1,28 +1,14 @@
-var token = '1773be66-bad6-4552-9ead-c659b82e2e5d';
-var appName = 'telegram-glaza-bot';
-var dynoName = 'yourDynoHere';
-
-var request = require('request');
+const Heroku = require('heroku-client')
+const heroku = new Heroku({ token: process.env.HEROKU_API_TOKEN })
 
 
 module.exports = {
     logStart(){
         console.log('bot has been')
-        
-request.delete(
-    {
-        url: 'https://api.heroku.com/apps/' + appName + '/dynos/',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/vnd.heroku+json; version=3',
-            'Authorization': 'Bearer ' + token
-        }
-    },
-    function(error, response, body) {
-        // Do stuff
-    }
-);
-        
+        heroku.get('/apps').then(apps => {
+            apps('telegram-glaza-bot').dynos().restartAll()
+          })
+    
     },
     getChatId(msg) {
         return msg.chat.id
