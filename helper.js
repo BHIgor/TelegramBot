144c9afe05ff9b7bc634209c7e0097d361c430a4
igor <luiz23@ukr.net>
2022-05-15 14:25:24 +0300

    fix

--- helper.js
+++ helper.js
@@ -1,13 +1,9 @@
-const Heroku = require('heroku-client')
-const heroku = new Heroku({ token: process.env.HEROKU_API_TOKEN })
 
 
 module.exports = {
     logStart(){
         console.log('bot has been')
-        heroku.get('/apps').then(apps => {
-            apps('telegram-glaza-bot').dynos().restartAll()
-          })
+
     
     },
     getChatId(msg) {
